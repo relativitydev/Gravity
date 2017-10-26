9e4e0dec384c56506c5c9bdc1466843a0ede5142
Jacob Malliet <jacobmalliet3@gmail.com>
2017-10-26 13:05:02 -0500

    Change ExecutionIdentity to be a public property on RsapiDao

--- Gravity/Gravity/DAL/RSAPI/RsapiDao.cs
+++ Gravity/Gravity/DAL/RSAPI/RsapiDao.cs
@@ -9,25 +9,26 @@ namespace Gravity.DAL.RSAPI
 
 	public partial class RsapiDao
 	{
+		public ExecutionIdentity CurrentExecutionIdentity { get; set; }
+
 		protected IHelper helper;
 		protected int workspaceId;
-		protected ExecutionIdentity currentExecutionIdentity;
+		
+		private InvokeWithRetryService invokeWithRetryService;
 
 		protected IRSAPIClient CreateProxy()
 		{
-			var proxy = helper.GetServicesManager().CreateProxy<IRSAPIClient>(this.currentExecutionIdentity);
+			var proxy = helper.GetServicesManager().CreateProxy<IRSAPIClient>(this.CurrentExecutionIdentity);
 			proxy.APIOptions.WorkspaceID = workspaceId;
 
 			return proxy;
 		}
-
-		private InvokeWithRetryService invokeWithRetryService;
-
+		
 		public RsapiDao(IHelper helper, int workspaceId, ExecutionIdentity executionIdentity, InvokeWithRetrySettings invokeWithRetrySettings = null)
 		{
 			this.helper = helper;
 			this.workspaceId = workspaceId;
-			this.currentExecutionIdentity = executionIdentity;
+			this.CurrentExecutionIdentity = executionIdentity;
 
 			if (invokeWithRetrySettings == null)
 			{
