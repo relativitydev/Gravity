77439b0bdd2923d4cee0630c67265608fdba94d6
Avi Levin <avi@milyli.com>
2018-06-06 13:21:16 -0500

    Cleanup/consolidate csproj files

--- Gravity/Gravity.Test/Properties/AssemblyInfo.cs
+++ Gravity/Gravity.Test/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
+
 [assembly: ComVisible(false)]
 [assembly: Guid("5a30bd81-511e-40d3-ba48-08399ae61a7b")]
