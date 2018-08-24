c29ea3316f2e27e6bb78bfc5810ea33aa7179177
Avi Levin <avi@milyli.com>
2018-08-24 11:55:21 -0500

    refactor: search lazily for child objects on deletion

--- Gravity/Gravity/DAL/RSAPI/RsapiDao.Delete.cs
+++ Gravity/Gravity/DAL/RSAPI/RsapiDao.Delete.cs
@@ -13,12 +13,6 @@ namespace Gravity.DAL.RSAPI
 		public void Delete<T>(int objectToDeleteId, ObjectFieldsDepthLevel depthLevel)
 			where T : BaseDto, new()
 		{
-			var scanRecursionLevel =
-				depthLevel == ObjectFieldsDepthLevel.OnlyParentObject
-					? ObjectFieldsDepthLevel.FirstLevelOnly //won't need to look past this to throw error
-					: ObjectFieldsDepthLevel.FullyRecursive;
-			T theObjectToDelete = Get<T>(objectToDeleteId, scanRecursionLevel);
-
 			var maxRecursionLevel =
 				depthLevel == ObjectFieldsDepthLevel.OnlyParentObject ? 0
 				: depthLevel == ObjectFieldsDepthLevel.FirstLevelOnly ? 1
@@ -28,7 +22,7 @@ namespace Gravity.DAL.RSAPI
 			var artifactsToDeleteList = new List<Tuple<int, int>>();
 
 			//populate artifacts to delete
-			PopulateArtifactsToDeleteList(artifactsToDeleteList, maxRecursionLevel, new[] { theObjectToDelete }, 0);
+			PopulateArtifactsToDeleteList<T>(artifactsToDeleteList, maxRecursionLevel, new[] { objectToDeleteId }, 0);
 
 			//order by items to delete by how deep in hierarchy they are
 			//so don't run into "can't delete" issues
@@ -46,30 +40,30 @@ namespace Gravity.DAL.RSAPI
 		internal void PopulateArtifactsToDeleteList<T>(
 			List<Tuple<int, int>> artifactsToDeleteList,
 			int maxRecursionLevel,
-			IList<T> artifacts,
+			IList<int> artifactIds,
 			int currentRecursionLevel) where T : BaseDto
 		{
-			if (!artifacts.Any())
+			if (!artifactIds.Any())
 				return;
 
 			if (currentRecursionLevel > maxRecursionLevel)
-				throw new ArgumentOutOfRangeException(nameof(maxRecursionLevel), "Exceeded maximum recursion level.");
+				throw new ArgumentOutOfRangeException(nameof(currentRecursionLevel), "Exceeded maximum recursion level.");
 
-			artifactsToDeleteList.AddRange(artifacts.Select(a => Tuple.Create(a.ArtifactId, currentRecursionLevel)));
-
+			artifactsToDeleteList.AddRange(artifactIds.Select(a => Tuple.Create(a, currentRecursionLevel)));
 
 			var childProperties = BaseDto.GetRelativityObjectChildrenListProperties<T>();
 
 			foreach (var propertyInfo in childProperties)
 			{
 				var childType = propertyInfo.PropertyType.GetEnumerableInnerType();
-				foreach (var parentObject in artifacts)
+				foreach (var artifactId in artifactIds)
 				{
-					var thisChildTypeObjs = propertyInfo.GetValue(parentObject, null) as IList;
+					//TODO: amend so can pass in all artifact IDs instead of looping over artifacts
+					var thisChildTypeIds = (List<int>)this.InvokeGenericMethod(childType, nameof(GetAllChildIds), artifactId);
 
 					//recurse with child objects and next recursion level
 					this.InvokeGenericMethod(childType, nameof(PopulateArtifactsToDeleteList),
-						artifactsToDeleteList, maxRecursionLevel, thisChildTypeObjs, currentRecursionLevel + 1);
+						artifactsToDeleteList, maxRecursionLevel, thisChildTypeIds, currentRecursionLevel + 1);
 				}
 			}
 
