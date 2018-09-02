431fafa3019c0da19abaf858659c30cce6361619
Avi Levin <avi@milyli.com>
2018-09-02 11:27:08 -0500

    Add bulk Update to RsapiDao interface

--- Gravity/Gravity/DAL/IGravityDao.cs
+++ Gravity/Gravity/DAL/IGravityDao.cs
@@ -8,10 +8,11 @@ namespace Gravity.DAL
 	{
 		void Delete<T>(int artifactID) where T : BaseDto, new();
 		void Delete<T>(T obj) where T : BaseDto;
-		List<T> Get<T>(IList<int> artifactIDs, ObjectFieldsDepthLevel depthLevel) where T : BaseDto, new();
 		T Get<T>(int artifactID, ObjectFieldsDepthLevel depthLevel) where T : BaseDto, new();
+		List<T> Get<T>(IList<int> artifactIDs, ObjectFieldsDepthLevel depthLevel) where T : BaseDto, new();
 		int Insert<T>(T obj, ObjectFieldsDepthLevel depthLevel) where T : BaseDto;
 		void Insert<T>(IList<T> objs, ObjectFieldsDepthLevel depthLevel) where T : BaseDto;
 		void Update<T>(T obj, ObjectFieldsDepthLevel depthLevel) where T : BaseDto;
+		void Update<T>(IList<T> objs, ObjectFieldsDepthLevel depthLevel) where T : BaseDto;
 	}
 }
